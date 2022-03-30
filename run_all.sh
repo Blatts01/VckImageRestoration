ae4e2221d9d4e236a08722cb92ac5cc88947e311
Blattmann <stefan.blattmann@ipm.fraunhofer.de>
2022-03-30 10:42:32 +0200

    adding Uformer and test script

--- run_all.sh
+++ run_all.sh
@@ -16,9 +16,9 @@ mkdir -p ${QAT_MODEL}
 mkdir -p ${LOG}/model
 
 # run training
-#python Uformer/train.py --arch UNet --batch_size 32 --gpu '0' \
-#    --train_ps 128 --train_dir /workspace/datasets/demoire/sidd/train/ --env 32_0705_1 \
-#    --val_dir /workspace/datasets/demoire/sidd/val/ --embed_dim 32 --warmup
+python Uformer/train.py --arch UNet --batch_size 32 --gpu '0' \
+    --train_ps 128 --train_dir /workspace/datasets/demoire/sidd/train/ --env 32_0705_1 \
+    --val_dir /workspace/datasets/demoire/sidd/val/ --embed_dim 32 --warmup
 
 # copy models and logs 
 #cp Uformer/log/UNet32_0705_1/*.txt ${LOG}/model/
@@ -46,10 +46,10 @@ mkdir -p ${LOG}/model
 #    --weights ${FLOAT_MODEL}/float_model.pth 2>&1 | tee ${LOG}/quant_fast_calib.log
 
 # qat training 
-python -u qat.py --arch UNet --batch_size 32 --gpu '0' \
-    --train_ps 128 --train_dir /workspace/datasets/demoire/sidd/train/ --env 32_0705_1 \
-    --val_dir /workspace/datasets/demoire/sidd/val/ --embed_dim 32 --warmup \
-    --save_dir ${QAT_MODEL} --qat_mode train
+#python -u qat.py --arch UNet --batch_size 32 --gpu '0' \
+#    --train_ps 128 --train_dir /workspace/datasets/demoire/sidd/train/ --env 32_0705_1 \
+#    --val_dir /workspace/datasets/demoire/sidd/val/ --embed_dim 32 --warmup \
+#    --save_dir ${QAT_MODEL} --qat_mode train
 
 #python -u qat.py --arch UNet --batch_size 32 --gpu '0' \
 #    --train_ps 128 --train_dir /workspace/datasets/demoire/sidd/train/ --env 32_0705_1 \
